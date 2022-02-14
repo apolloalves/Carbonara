c4fd3c85695292950e1089b5b9f5e6c6ed0bfe72
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-02-13 23:32:53 -0300

    Renamed fix__packages-5.1.sh

--- ext__disk.sh
+++ ext__disk.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
-echo -n "Deseja montar o disco externo do timeshif (y/n)"
-read question 
+echo -n "Deseja montar o disco externo do timeshift (y/n)"
+read -r EXT_timeshift 
 
-if test "y" = "$question"
-then 
-   sudo mount /dev/sdd7 /mnt/EXT__ST500LM012_BK_TIMESHIFT/
-   echo "EXT__ST500LM012_BK_TIMESHIFT está montado!"
-else 
-   sudo umount /dev/sdd7 /mnt/EXT__ST500LM012_BK_TIMESHIFT/ -l
-   #echo "EXT__ST500LM012_BK_TIMESHIFT foi desmontado!"
+if test "y" = "$EXT_timeshift"
+
+    then 
+    #sudo mount /dev/sdd7 /mnt/EXT__ST500LM012_BK_TIMESHIFT/
+    echo "EXT__ST500LM012_BK_TIMESHIFT está montado!"
+
+elif test "n" = "$EXT_timeshift"
+    
+    then
+    #sudo umount /dev/sdd7 /mnt/EXT__ST500LM012_BK_TIMESHIFT/ -l
+    echo "EXT__ST500LM012_BK_TIMESHIFT não montado!"
+
+elif test "y" || "n" != "$EXT_timeshift"
+    then
+    echo 'invalid arguments!'
 
 fi
 
+echo "Deseja montar o disco externo do clonezilla? (y/n)"
+read -r EXT_clonezilla
+
+if test "y" = "$EXT_clonezilla"
+
+    then
+        #sudo mount /dev/sdd6 /mnt/EXT__ST500LM012_CLONEZIBK__RAID/ 
+        echo "EXT__ST500LM012_CLONEZIBK__RAID está montado!"
+    
+    
+    elif test "n" = "$EXT_clonezilla"
+        then
+            #sudo umount /dev/sdd6 /mnt/EXT__ST500LM012_CLONEZIBK__RAID/ -l
+            echo "EXT__ST500LM012_CLONEZIBK__RAID foi desmontado"
+
+    elif test "y" || "n" != "$EXT_clonezilla"
+    then
+    echo 'invalid arguments!'
 
 
-echo "Deseja montar o dosco externo do clonezilla? (y/n)"
-read question2
-
-if test "y" = "$question2"
-then
-    sudo mount /dev/sdd6 /mnt/EXT__ST500LM012_CLONEZIBK__RAID/ 
-    echo "EXT__ST500LM012_CLONEZIBK__RAID está montado!"
-else
-    sudo umount /dev/sdd6 /mnt/EXT__ST500LM012_CLONEZIBK__RAID/ -l
-    #echo "EXT__ST500LM012_CLONEZIBK__RAID foi desmontado"
 fi
 
 
