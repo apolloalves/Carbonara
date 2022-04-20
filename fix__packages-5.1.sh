fd54acf4663ef2d3654ad800ddfa22d7deddcda0
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-04-20 12:48:53 -0300

    Decision making with until

--- fix__packages-5.1.sh
+++ fix__packages-5.1.sh
@@ -34,11 +34,11 @@ echo -e '***********************************************************************
 	sudo apt update --fix-missing
 	sudo apt --fix-broken install
 	sudo apt install -f
-        sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d')
-	sudo apt autoremove -y
+      sudo apt-get remove --purge $(dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d') -y
 	sudo apt --purge autoremove -y
-	sudo apt autoclean
-	sudo apt clean
+	sudo apt autoremove -y
+	sudo apt autoclean -y
+	sudo apt clean -y 
 	sudo apt remove $(deborphan)
 
 #OK
@@ -70,7 +70,6 @@ echo -e "\033[01;32mCLEANING UP CACHE ENTRIES...\033[01;37m"
 	sudo sync; echo 1 > /proc/sys/vm/drop_caches
 	sudo sync; echo 2 > /proc/sys/vm/drop_caches
 	sudo sync; echo 3 > /proc/sys/vm/drop_caches
-
 
 echo -e "\033[01;32mFIXINIG DPKGS BROKENS\033[01;37m!\n"
 echo '**************************************************************************************************************'
@@ -135,58 +134,73 @@ echo '**************************************************************************
 #STACER/TIMESHIFT CONDITIONAL
 #********************************************************************************************************************
 echo -n 'Do you want to open stacer? (y/n)'
-read -r stacer_question
+ read -r stacer_question
 
 
-if test "y" = "$stacer_question"
+ if test "y" = "$stacer_question"
 
-   then
+    then
 
-         echo "initializing stacer..."
-         sleep 2
-         sudo stacer &        
-         echo -e '\n\033[00;37m[\033[00;32m OK!\033[00;37m ]\033m\n'
-         neofetch
-   
-   elif test "n" = "$stacer_question"
-     
-      then
-      echo -n 'Can I create a new snapshot containing the current state of the system? (y/n)'
-      read -r timeshift_question
+      echo "stacer is running..."
+      sleep 1
+      stacer &        
+
+      until ! pgrep -x "stacer" > /dev/null
+      do
+            echo -e '\cAguardando o encerramento do stacer pelo usuário...'
+            sleep 1
+      done
+
+
+echo -n 'Can I create a new snapshot containing the current state of the system? (y/n)'
+read -r timeshift_question
         
-         if test "y" = "$timeshift_question"
 
-            then
+if test "y" = "$timeshift_question"
 
-               echo "initializing timeshift..."
-               sleep 2
-               sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.0.sh ]' --tags D
-               echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
-               echo -e "\033[01;31mCreating snapshot...\033[00;37m"
-               neofetch 
-                                
-         elif test "n" = "$timeshift_question"
-                        
-               then 
-               echo "Generating the list of snapshots..."
-               sleep 2
-               sudo timeshift --delete
-               neofetch
-         
-         elif
-               test "y" || "n" != "$stacer_question" 
-               then
-               echo "invalid arguments!"   
-           
-         fi
-
-   elif test "y" || "n" != "$stacer_question"
       then
-      echo "Invalid arguments!"   
 
+            echo "initializing timeshift..."
+            sleep 2
+            sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.2.sh ]' --tags D
+            echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
+            echo -e "\033[01;31mCreating snapshot...\033[00;37m"
+            neofetch 
+                              
+      elif test "n" = "$timeshift_question"
+                  
+            then 
+            echo "Generating the list of snapshots..."
+            sleep 2
+            sudo timeshift --delete
+            neofetch
+      
+      elif
+            test "y" || "n" != "$stacer_question" 
+            then
+            echo "invalid arguments!"   
+      
+      fi
 
 fi
 
 
+if test "n" = "$stacer_question" 
 
+      then 
 
+            echo "initializing timeshift..."
+            sleep 2
+            sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.2.sh ]' --tags D
+            echo -e "\033[01;31mCreating snapshot...\033[00;37m"
+            echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
+            echo "Generating the list of snapshots..."
+            sleep 2
+            sudo timeshift --delete
+            neofetch
+
+            elif test "y" || "n" != "$stacer_question" || test "y" || "n" != "$timeshift_question"
+            then
+            echo "Invalid arguments!"   
+
+fi
--- stacer.sh
+++ stacer.sh
@@ -1,52 +1,72 @@
-echo -n 'Do you want to open stacer? (y/n)'
-read -r stacer_question
+#!/bin/bash
+ echo -n 'Do you want to open stacer? (y/n)'
+ read -r stacer_question
 
 
-if test "y" = "$stacer_question"
+ if test "y" = "$stacer_question"
 
-   then
+    then
 
-         echo "initializing stacer..."
-         sleep 2
-         sudo stacer &        
-         echo -e '\n\033[00;37m[\033[00;32m OK!\033[00;37m ]\033m\n'
-         neofetch
-   
-   elif test "n" = "$stacer_question"
-     
-      then
-      echo -n 'Can I create a new snapshot containing the current state of the system? (y/n)'
-      read -r timeshift_question
+      echo "stacer is running..."
+      sleep 1
+      stacer &        
+
+      until ! pgrep -x "stacer" > /dev/null
+      do
+            echo -e '\cAguardando o encerramento do stacer pelo usuário...'
+            sleep 1
+      done
+
+
+echo -n 'Can I create a new snapshot containing the current state of the system? (y/n)'
+read -r timeshift_question
         
-         if test "y" = "$timeshift_question"
 
-            then
+if test "y" = "$timeshift_question"
 
-               echo "initializing timeshift..."
-               sleep 2
-               sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.0.sh ]' --tags D
-               echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
-               echo -e "\033[01;31mCreating snapshot...\033[00;37m"
-               neofetch 
-                                
-         elif test "n" = "$timeshift_question"
-                        
-               then 
-               echo "Generating the list of snapshots..."
-               sleep 2
-               sudo timeshift --delete
-               neofetch
-         
-         elif
-               test "y" || "n" != "$stacer_question" 
-               then
-               echo "invalid arguments!"   
-           
-         fi
-
-   elif test "y" || "n" != "$stacer_question"
       then
-      echo "Invalid arguments!"   
 
+            echo "initializing timeshift..."
+            sleep 2
+            sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.2.sh ]' --tags D
+            echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
+            echo -e "\033[01;31mCreating snapshot...\033[00;37m"
+            neofetch 
+                              
+      elif test "n" = "$timeshift_question"
+                  
+            then 
+            echo "Generating the list of snapshots..."
+            sleep 2
+            sudo timeshift --delete
+            neofetch
+      
+      elif
+            test "y" || "n" != "$stacer_question" 
+            then
+            echo "invalid arguments!"   
+      
+      fi
+
+fi
+
+
+if test "n" = "$stacer_question" 
+
+      then 
+
+            echo "initializing timeshift..."
+            sleep 2
+            sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.2.sh ]' --tags D
+            echo -e '\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n'
+            echo -e "\033[01;31mCreating snapshot...\033[00;37m"
+            echo "Generating the list of snapshots..."
+            sleep 2
+            sudo timeshift --delete
+            neofetch
+
+            elif test "y" || "n" != "$stacer_question" || test "y" || "n" != "$timeshift_question"
+            then
+            echo "Invalid arguments!"   
 
 fi
