ba13d22d39699fdb8bc49cbfd750eba773e81c25
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-08-19 22:10:03 -0300

    Removed ]

--- atk.sh
+++ atk.sh
@@ -3,3 +3,7 @@ sudo mount /dev/sdc8 /mnt/ATK__HAIRY/
 nautilus /mnt/ATK__HAIRY/. &
 echo -e "\n\033[00;37m[\033[00;32m DONE!\033[00;37m ]\033m\n"
 
+#!/bin/bash
+sudo mount /dev/sdc8 /mnt/ATK__HAIRY/
+nautilus /mnt/ATK__HAIRY/. &
+echo -e "\n\033[00;37m[\033[00;32m DONE!\033[00;37m ]\033m\n"
