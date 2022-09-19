5fe846b3b46236e100f0266e84ed29779ae7a9a0
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-09-19 20:09:09 -0300

    fix__packages-5.3.sh

--- atk.sh
+++ atk.sh
@@ -6,4 +6,4 @@ echo -e "\n\033[00;37m[\033[00;32m DONE!\033[00;37m ]\033m\n"
 #!/bin/bash
 sudo mount /dev/sdc8 /mnt/ATK__HAIRY/
 nautilus /mnt/ATK__HAIRY/. &
-echo -e "\n\033[00;37m[\033[00;32m DONE!\033[00;37m ]\033m\n"
+echo -e "\n\033[00;37m[\033[00;32m ON!\033[00;37m ]\033m\n"
