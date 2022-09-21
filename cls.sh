07ddd9be04e407b91ed584b54a6a065a9888e9ac
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-09-21 07:36:16 -0300

    Trash Empty command new

--- cls.sh
+++ cls.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sudo rm -rfv  /home/apollo__nicolly/.local/share/recently-used.xbel
+sudo rm -rfv /mnt/ATK__HAIRY/.Trash*
 sudo umount -l -f /dev/sdc8 /mnt/ATK__HAIRY/ 
-sudo trash-empty --all -f
+#Trash-cli - is no longer supported
+#sudo trash-empty --all -f
+
+sudo rm -rf ~/.local/share/Trash/*i
+
+
 nautilus --quit
 
 echo -e "\n\033[01;37m[\033[00;32m OK\033[00;37m ]\033m\n"
