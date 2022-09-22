7a393e913dbeb9028834ff9bbcf28bfd343a8781
APOLLO ALVES <apolloapolloalves@gmail.com>
2022-09-22 12:41:19 -0300

    Refactor logic stacer conditional

--- stacer.sh
+++ stacer.sh
@@ -2,7 +2,7 @@
 VALIDATE=$(test "y" || "n" != "$stacer_question" || test "y" || "n" != "$timeshift_question")
 
 
- echo -n 'Do you want to open stacer? ( y/n ) '
+ echo -n 'Do you want to open stacer ( y/n ) ? or press any key to exit '
  read -r stacer_question
 
 
@@ -11,14 +11,14 @@ VALIDATE=$(test "y" || "n" != "$stacer_question" || test "y" || "n" != "$timeshi
     then
 
             echo "stacer is running..."
-            sleep 1
+            sleep 2
             stacer &        
 
       until ! pgrep -x "stacer" > /dev/null
       
       do
             echo -e '\cAguardando o encerramento do stacer pelo usuário...'
-            sleep 1
+            sleep 2
       done
 
       elif test "n" = "$stacer_question"
@@ -33,14 +33,14 @@ VALIDATE=$(test "y" || "n" != "$stacer_question" || test "y" || "n" != "$timeshi
             echo "Generating the list of snapshots..."
             sleep 2
             sudo timeshift --delete
-           
+            neofetch
       else 
             echo "${VALIDATE}"
-            echo "Invalid arguments!"
+            echo -e "The arguments are invalid or the operation was canceled by the user\n"
 fi
 
-
-echo -n 'Can I create a new snapshot containing the current state of the system? ( y/n ) '
+echo -e "\n"
+echo -n 'Can I create a new snapshot containing the current state of the system ( y/n ) ? or press any key to exit '
 read -r timeshift_question
         
 
@@ -53,7 +53,11 @@ if test "y" = "$timeshift_question"
             sudo timeshift --create --verbose --comments 'shell : [ fix__packages-5.2.sh ]' --tags D
             echo -e "\n\033[00;37m[\033[00;32m done!\033[00;37m ]\033m\n"
             echo -e "\033[01;31mCreating snapshot...\033[00;37m"
+            echo -e '\n************************************************************************************************************\n'
+            lsb_release -a      
+            echo -e '\n************************************************************************************************************\n'
             neofetch 
+
                               
       elif test "n" = "$timeshift_question"
                   
@@ -61,11 +65,14 @@ if test "y" = "$timeshift_question"
             echo "Generating the list of snapshots..."
             sleep 2
             sudo timeshift --delete
+            echo -e '\n************************************************************************************************************\n'
+            lsb_release -a      
+            echo -e '\n************************************************************************************************************\n'
+            neofetch 
                   
       elif
             echo "${VALIDATE}"
             then
-            echo "invalid arguments!"   
+            echo -e "The arguments are invalid or the operation was canceled by the user\n"   
       
       fi
-
