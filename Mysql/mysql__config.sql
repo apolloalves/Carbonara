4fa890ce6487a79f6867c071c83e27e4e7656cb9
apolloalves <apolloalves@gmail.com>
2023-01-25 15:11:29 -0300

    Resolved error 1045 mysql - auth [ ok ]

--- Mysql/mysql__config.sql
+++ Mysql/mysql__config.sql
@@ -1,11 +1,16 @@
-  
-            USE mysql;
-            UPDATE user SET plugin='mysql_native_password' WHERE User='root';
-            -- 2
-            CREATE USER 'apollo'@'localhost' IDENTIFIED BY '3109';
-            GRANT ALL PRIVILEGES ON *.* TO 'apollo'@'localhost';
-            UPDATE user SET plugin='auth_socket' WHERE User='apollo';
-            FLUSH PRIVILEGES;
-            SELECT User, Host, plugin FROM mysql.user;   
-            FLUSH PRIVILEGES;
+/*************************************************************
+ * Script details : for mysl autentication : root and apollo *
+ * Author : Apollo Alves                                     *
+ * Create : 25/01/2023                                       *
+ *************************************************************/
+
+    USE mysql;
+    UPDATE user SET plugin='mysql_native_password' WHERE User='root';
+
+    CREATE USER 'apollo'@'localhost' IDENTIFIED BY '3109';
+    GRANT ALL PRIVILEGES ON *.* TO 'apollo'@'localhost';
+    UPDATE user SET plugin='auth_socket' WHERE User='apollo';
+    FLUSH PRIVILEGES;
+    SELECT User, Host, plugin FROM mysql.user;   
+    FLUSH PRIVILEGES;
             
