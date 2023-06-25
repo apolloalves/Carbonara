48df89834486fab8e8133da6b545447b03679686
apolloalves <apolloalves@gmail.com>
2023-06-25 11:01:53 -0300

    Removed EXT__STORAGE

--- System/bin/btrfs_balance.sh
+++ System/bin/btrfs_balance.sh
@@ -1,29 +1,31 @@
-    #!/bin/bash
+#!/bin/bash
 
-    #####################################################################
-    #                                                                   #
-    # Script: Script for file balancing for BTRFS partitions.           #
-    #                                                                   #
-    # Author: Apollo Alves                                              #
-    # Date: 03/06/2023                                                  #
-    #                                                                   #
-    #####################################################################
+#####################################################################
+#                                                                   #
+# Script: Script for file balancing for BTRFS partitions.           #
+#                                                                   #
+# Author: Apollo Alves                                              #
+# Date: 03/06/2023                                                  #
+#                                                                   #
+#####################################################################
 
+MENU='shellCare-2.0.1.sh'
 
-    YES="y"
-    NO="n"
-    echo 
-    echo -n 'Do you want to balance the volume "/" (y/n)? '
-    read -r ROOTVOLUME
+YES="y"
+NO="n"
 
-    if [ "$YES" = "$ROOTVOLUME" ]; then
-    
+echo
+echo -n 'Do you want to balance the volume "/" (y/n)? '
+read -r ROOTVOLUME
+
+if [ "$YES" = "$ROOTVOLUME" ]; then
+
     echo -e "\033[01;32m Opening tab of btrfs balance root volume...\033[00;37m"
     sleep 1
     gnome-terminal --tab -- bash -c "time sudo btrfs balance start /; exec bash"
     echo -e "\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n"
 
-    elif [ "$NO" = "$ROOTVOLUME" ]; then
+elif [ "$NO" = "$ROOTVOLUME" ]; then
     echo -n 'Do you want to balance the volume "/home" (y/n)? '
     read -r HOMEVOLUME
 
@@ -33,33 +35,37 @@
         echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
 
     elif [ "$NO" = "$HOMEVOLUME" ]; then
-    echo -n 'Do you want to balance the volume "/EXT@ST500LM012__CLONRAID/" (y/n)? '
-    read -r CLONRAID
+        echo -n 'Do you want to balance the volume "/EXT@ST500LM012__CLONRAID/" (y/n)? '
+        read -r CLONRAID
 
-    if [ "$YES" = "$CLONRAID" ]; then
-    
-        gnome-terminal --tab -- bash -c "time sudo btrfs balance start /mnt/EXT@ST500LM012__CLONRAID/; exec bash"
-        echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
+        if [ "$YES" = "$CLONRAID" ]; then
 
-    elif [ "$NO" = "$CLONRAID" ]; then
-    echo -n 'Do you want to balance the volume "/EXT@ST500LM012__STORAGE/" (y/n)? '
-    read -r STORAGE
+            gnome-terminal --tab -- bash -c "time sudo btrfs balance start /mnt/EXT@ST500LM012__CLONRAID/; exec bash"
+            echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
 
-    if [ "$YES" = "$STORAGE" ]; then
-    
-        gnome-terminal --tab -- bash -c "time sudo btrfs balance start /mnt/EXT@ST500LM012__STORAGE/; exec bash"
-        echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
-    
-    elif [ "$NO" = "$STORAGE" ]; then
-    
-    echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
-    shellCare-1.2.sh
-    
-            fi
+        elif [ "$NO" = "$CLONRAID" ]; then
+            # echo -n 'Do you want to balance the volume "/EXT@ST500LM012__STORAGE/" (y/n)? '
+            # read -r STORAGE
+
+            # if [ "$YES" = "$STORAGE" ]; then
+
+            #     gnome-terminal --tab -- bash -c "time sudo btrfs balance start /mnt/EXT@ST500LM012__STORAGE/; exec bash"
+            #     echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
+
+            # elif [ "$NO" = "$STORAGE" ]; then
+
+            echo -e '\n\033[01;37m[\033[00;32m OK\033[01;37m ]\033m\n'
+            $MENU
+
         fi
     fi
 fi
 
- echo "${VALIDATE}"
-        echo -e "The arguments are invalids!\n"
-        shellCare-1.2.sh
+if
+    echo "${VALIDATE}"
+then
+
+    echo -e "command not found!\n"
+    $MENU
+
+fi
--- System/bin/shellCare-2.0.1.sh
+++ System/bin/shellCare-2.0.1.sh
@@ -214,7 +214,7 @@ function checkSpace {
      echo -e "\n Check spaces...\n"
      #####################################################################################################################
      sleep 1
-     df -h / && echo "" && df -h /home && echo "" && df -h /mnt/EXT@ST500LM012__CLONRAID && echo "" && df -h /mnt/EXT@ST500LM012__STORAGE/
+     df -h / && echo "" && df -h /home && echo "" && df -h /mnt/EXT@ST500LM012__CLONRAID
      echo -e "\n\033[01;37m[\033[00;32m OK\033[00;37m ]\033m\n"
 
 }
