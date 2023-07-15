83cfab730d4054652c074221d75546d69fc569ac
apolloalves <apolloalves@gmail.com>
2023-07-14 21:56:25 -0300

    description for file removePack.sh

--- System/bin/removePack.sh
+++ System/bin/removePack.sh
@@ -2,13 +2,36 @@
 
 #####################################################################
 #                                                                   #
-# Script: removepack.sh For updates packages of system and   #
-# optimizer performance.                                            #
-#                                                                   #
+# Script: removepack.sh                                             #
 # Author: Apollo Alves                                              #
-# Date: 02/07/2023                                                  #
+# Date: 14/07/2023                                                  #
 #                                                                   #
 #####################################################################
+
+########################################################################################################################
+#                                                                                                                      #
+# This shell script called "removepack.sh" presents a menu for cleaning up packages from the system. Here is a summary #
+# of what it does:                                                                                                     #
+#                                                                                                                      #
+# 1. Define some variables, like MENU and REMOVEPACK, which are the names of other shell scripts.                      #
+#                                                                                                                      #
+# 2. Asks the user if he wants to clean up system packages.                                                            #
+# - The user can answer "y" (yes) or "n" (no).                                                                         #
+#                                                                                                                      #
+# 3. If the user answers "y", the script executes the "$REMOVEPACK" script, which is responsible for cleaning up the   #
+# system packages.                                                                                                     #
+#                                                                                                                      #
+# 4. If the user answers "n", the script displays the message "skipped!" and runs the "$MENU" script, which is         #
+# probably a main menu or next step.                                                                                   #
+#                                                                                                                      #
+# 5. If the user provides an answer other than "y" or "n", the script displays an invalid input message and returns    #
+# to the "$MENU" menu.                                                                                                 #
+#                                                                                                                      #
+# In short, this script presents a menu for cleaning up system packages and runs the cleanup script only if the        #
+# user chooses to do so. Otherwise, it skips cleaning and proceeds to the next step in the menu.                       #
+#                                                                                                                      #
+########################################################################################################################
+
 
 MENU='shellCare-22.0.1.sh'
 REMOVEPACK='remove.sh'
