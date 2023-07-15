654bf23bab0977019c9ec4ddc3fa800624bee3e2
apolloalves <apolloalves@gmail.com>
2023-07-14 21:12:35 -0300

    description for file checkSpace.sh

--- System/bin/checkSpace.sh
+++ System/bin/checkSpace.sh
@@ -2,16 +2,28 @@
 
 #####################################################################
 #                                                                   #
-# Script: checkSpace                                                #
-#                                                                   #
+# Script: checkSpace.sh                                             #
 # Author: Apollo Alves                                              #
 # Date: 14/07/2023                                                  #
 #                                                                   #
 #####################################################################
 
-#####################################################################
+#####################################################################################################################
+#                                                                                                                   #
+# Description: This shell script called "checkSpace.sh" performs some disk space checks. Here is a summary of what  # 
+# it does:                                                                                                          #
+# 1. Displays a space check message.                                                                                #
+# 2. Wait 1 second.                                                                                                 #
+# 3. Run the commands "df -h /", "df -h /home" and "df -h /mnt/EXT@ST500LM012__CLONRAID" to display disk space      #
+# information in the root, /home and /mnt/EXT@ directories ST500LM012__CLONRAID, respectively.                      #
+# 4. Displays a message indicating that the verification completed successfully.                                    #
+#                                                                                                                   #
+# Basically, this script prints information about available disk space in the mentioned directories. It can be used #
+# to monitor disk space and verify that enough space is available.                                                  #
+#                                                                                                                   #
+#####################################################################################################################
+
 echo -e "\n Check spaces...\n"                                      
-#####################################################################
 
 sleep 1
 df -h / && echo "" && df -h /home && echo "" && df -h /mnt/EXT@ST500LM012__CLONRAID
