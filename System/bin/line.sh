07fac2feaac2a8d0f0ee32a1b1c2b1dc33db3cfa
apolloalves <apolloalves@gmail.com>
2023-07-14 21:37:07 -0300

    description for file line.sh

--- System/bin/line.sh
+++ System/bin/line.sh
@@ -4,20 +4,19 @@
 # Script: line.sh - Enter a line after each command                     #           
 #                                                                       #
 # Author: Apollo Alves                                                  #
-# Date: 11/06/2023                                                      #
+# Date: 14/07/2023                                                      #
 #                                                                       #
 #########################################################################
 
 #############################################################################################################
-#                                                                                                           #
-# This bash code creates a line containing 90 trace characters (─) and prints this line on the console.     #
+# Description : This bash code creates a line containing 90 trace characters (─) and prints this line on    #
+# the console.                                                                                              #
 # Here is a step by step description of what the code does:                                                 #
 #                                                                                                           #
 # The line variable is initialized as an empty string:                                                      #
 # Then a loop is used to ite from 0 to 89 (90 times) and build the line by adding the character "─" (trace) #
 # to the line variable with each iteration:                                                                 #
 # Finally, the Printf command is used to print the line on the console.                                     #
-#                                                                                                           #
 #                                                                                                           #
 #############################################################################################################
 
