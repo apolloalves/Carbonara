6a4609a0e4cdc34f65f564d18f5bcc43ddf71520
apolloalves <apolloalves@gmail.com>
2023-07-16 18:56:38 -0300

    included broot_install.sh

--- install.sh
+++ install.sh
@@ -89,23 +89,7 @@ if [ -n "$path" ]; then
     source ~/.bashrc
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
     echo
-    
-    echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
-    
-    sudo rm -rfv /usr/local/bin/broot/conf-new-style.hjson
-    sudo rm -rfv /usr/local/bin/broot/system.conf
-    sudo rm -rfv /usr/local/bin/broot
-    
-    source ~/.bashrc
-
-    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
-
-    curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
-    sleep 5
-    sudo mv -v broot /usr/local/bin
-    sudo chmod +x /usr/local/bin/broot
-    source ~/.bashrc
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    broot_install.sh  
     sleep 3
     shellCare-22.0.1.sh
 
