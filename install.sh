e0dfe616d4153ffe0135a34681e37840d45d8b7b
apolloalves <apolloalves@gmail.com>
2023-07-16 18:35:49 -0300

    hotfix alers installs apps

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ if [ -n "$path" ]; then
     ls -lsht $path/System/bin/*.sh
     find $path/System/bin/ -ls | wc -l
 
-    echo -e "\n\033[01;32msetting execute permissions on shell files...\033[00;37m\n"
+    echo -e "\n\033[01;32msetting execute permissions on shell files...\033[00;37m"
     sleep 2
     sudo chmod +x $path/System/bin/*.sh
     echo -e "\n\033[01;32mcopying the executables to the /bin folder...\033[00;37m\n"
@@ -91,13 +91,17 @@ if [ -n "$path" ]; then
     
     echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
     
+    sudo rm -rfv/usr/local/bin/broot/conf-new-style.hjson
+    sudo rm -rfv /usr/local/bin/broot/system.conf
     sudo rm -rfv /usr/local/bin/broot
+    
     source ~/.bashrc
 
     echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
 
     curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
-    sudo mv broot /usr/local/bin
+    sleep 5
+    sudo mv -v broot /usr/local/bin
     sudo chmod +x /usr/local/bin/broot
     source ~/.bashrc
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
