7f6acf149a29bd0164e9c7b7f4a79a0b50f3fbd9
apolloalves <apolloalves@gmail.com>
2023-07-16 18:56:10 -0300

    fix broot_install.sh

--- System/bin/broot_install.sh
+++ System/bin/broot_install.sh
@@ -10,12 +10,19 @@
 #                                                                   #
 #####################################################################
 
-curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
+    echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
+    
+    sudo rm -rfv /usr/local/bin/broot/conf-new-style.hjson
+    sudo rm -rfv /usr/local/bin/broot/system.conf
+    sudo rm -rfv /usr/local/bin/broot
+    
+    source ~/.bashrc
 
-sudo mv broot /usr/local/bin
-sudo chmod +x /usr/local/bin/broot
-source ~/.bashrc
-echo "broot was installed!"
+    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
 
-
-
+    curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
+    sleep 5
+    sudo mv -v broot /usr/local/bin
+    sudo chmod +x /usr/local/bin/broot
+    source ~/.bashrc
+    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
