64a0f8e4fa8ee76964c65325ba86c719a2bb7b11
apolloalves <apolloalves@gmail.com>
2023-07-16 19:22:57 -0300

    include new tab for broot directory

--- System/bin/shellCare-22.0.1.sh
+++ System/bin/shellCare-22.0.1.sh
@@ -154,7 +154,7 @@ if [ "$option_choice" != "q" ]; then
               ;;
           9)
               echo -e "\nOpening broot...\n"
-              sudo broot -s -p -d --sort-by-date '/'
+              gnome-terminal --tab -- bash -c "sudo broot -s -p -d --sort-by-date '/mnt/VENTOY/' ; exec bash"
               sleep 1
               ;;
 
