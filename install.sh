64087fa7c897cb0d6476cba98d2b758267824abb
apolloalves <apolloalves@gmail.com>
2023-07-16 18:29:21 -0300

    hotfix alers installs apps

--- install.sh
+++ install.sh
@@ -89,9 +89,14 @@ if [ -n "$path" ]; then
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
     echo
     
-    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
-    curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
+    echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
+    
+    sudo rm -rfv /usr/local/bin/broot
+    source ~/.bashrc
 
+    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
+
+    curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
     sudo mv broot /usr/local/bin
     sudo chmod +x /usr/local/bin/broot
     source ~/.bashrc
