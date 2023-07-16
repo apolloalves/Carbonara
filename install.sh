038aec582a685efae5c6e9d50973ede2bed395b5
apolloalves <apolloalves@gmail.com>
2023-07-16 18:22:46 -0300

    hotfix alers installs apps

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Defines the root directory from which find will be run
 ROOT_DIR="/"
-echo -e "\nwait...\n"
+echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
 
 # Use the find command to locate the "gnu-bash" folder
 path=$(find "$ROOT_DIR" -type d -name "gnu-bash" 2>/dev/null)
@@ -23,7 +23,7 @@ if [ -n "$path" ]; then
     echo -e "\n\033[01;32mcopying the executables to the /bin folder...\033[00;37m\n"
     sleep 2
     sudo rsync -avh $path/System/bin/*.sh /bin/
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     # arrumar que bra de linmha
     echo -e "\n\033[01;32mupdate packages of system...\033[00;37m\n"
@@ -32,71 +32,70 @@ if [ -n "$path" ]; then
     sudo apt update -y && sudo apt upgrade -y
     sudo dpkg --configure -a
     sudo apt install -f
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
-    echo -e "\n\033[01;32installing dependencies...\033[00;37m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    echo -e "\n\033[01;32minstalling dependencies...\033[00;37m\n"
     sleep 3
-    echo -e "\n\033[01;32installing neofetch...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling neofetch...\033[00;37m\n"
     
     sudo apt install neofetch -y
     neofetch
     echo
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing deborphan...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling deborphan...\033[00;37m\n"
     
     sudo apt install deborphan -y
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing preload...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling preload...\033[00;37m\n"
     sleep 2
     sudo apt install preload
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing prelink...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling prelink...\033[00;37m\n"
     sleep 2
     sudo apt install prelink -y
     sed 's/PRELINKING=unknown/PRELINKING=yes/g' prelink-sed >/dev/null
     sudo prelink -amvR
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing trash-cli...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling trash-cli...\033[00;37m\n"
     sleep 2
     sudo apt-get install trash-cli
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing stacer...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling stacer...\033[00;37m\n"
     sleep 2
     sudo apt install stacer -y
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing Eggs...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling Penguins-eggs...\033[00;37m\n"
     curl -fsSL https://pieroproietti.github.io/penguins-eggs-ppa/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/penguins-eggs.gpg
     echo "deb [arch=$(dpkg --print-architecture)] https://pieroproietti.github.io/penguins-eggs-ppa ./" | sudo tee /etc/apt/sources.list.d/penguins-eggs.list >/dev/null
     sudo apt update -y
     sleep 2
     sudo apt install eggs=9.4.15 -y
     sudo apt-mark hold eggs=9.4.15
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32installing flatpak...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling flatpak...\033[00;37m\n"
     sleep 2
     sudo apt install flatpak -y
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32inserting history format in .bashrc\033[00;37m\n"
+    echo -e "\n\033[01;32minserting history format in ~/.bashrc\033[00;37m\n"
     sed -i "14s/.*/HISTTIMEFORMAT='%Y-%m-%d%T '/" ~/.bashrc
     source ~/.bashrc
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
     echo
     
-    echo -e "\n\033[01;32installing broot...\033[00;37m\n"
+    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
     curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
 
     sudo mv broot /usr/local/bin
     sudo chmod +x /usr/local/bin/broot
     source ~/.bashrc
-    echo "broot was installed!"
-    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\033[01;37m\033[01;32mdone\033[00;37m\033m"
     sleep 3
     shellCare-22.0.1.sh
 
