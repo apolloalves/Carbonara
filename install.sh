0f46f355d8f7dc17dd2a2a6ea83f1ea9a59dde62
apolloalves <apolloalves@gmail.com>
2023-07-16 18:11:09 -0300

    refactor install.sh

--- install.sh
+++ install.sh
@@ -12,93 +12,92 @@ path=$(find "$ROOT_DIR" -type d -name "gnu-bash" 2>/dev/null)
 if [ -n "$path" ]; then
 
     sleep 2
-    echo "The cloned 'gnu-bash' folder was found in: $path"
+    echo -e "\n\033[01;32mThe cloned 'gnu-bash' folder was found in: $path\033[00;37m\n"
 
     ls -lsht $path/System/bin/*.sh
     find $path/System/bin/ -ls | wc -l
 
-    echo -e "\nsetting execute permissions on shell files...\n"
+    echo -e "\n\033[01;32msetting execute permissions on shell files...\033[00;37m\n"
     sleep 2
     sudo chmod +x $path/System/bin/*.sh
-    echo -e "copying the executables to the /bin folder...\n"
+    echo -e "\n\033[01;32mcopying the executables to the /bin folder...\033[00;37m\n"
     sleep 2
     sudo rsync -avh $path/System/bin/*.sh /bin/
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
     # arrumar que bra de linmha
-    echo "update packages of system..."
-    echo "wait..."
+    echo -e "\n\033[01;32mupdate packages of system...\033[00;37m\n"
+    echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
     sleep 2
     sudo apt update -y && sudo apt upgrade -y
     sudo dpkg --configure -a
     sudo apt install -f
-    echo "done!"
-    echo "installing dependencies..."
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    echo -e "\n\033[01;32installing dependencies...\033[00;37m\n"
     sleep 3
-    echo
-    echo "installing neofetch..."
+    echo -e "\n\033[01;32installing neofetch...\033[00;37m\n"
+    
     sudo apt install neofetch -y
     neofetch
     echo
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing deborphan..."
+    echo -e "\n\033[01;32installing deborphan...\033[00;37m\n"
+    
     sudo apt install deborphan -y
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing preload..."
+    echo -e "\n\033[01;32installing preload...\033[00;37m\n"
     sleep 2
     sudo apt install preload
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing prelink..."
+    echo -e "\n\033[01;32installing prelink...\033[00;37m\n"
     sleep 2
     sudo apt install prelink -y
     sed 's/PRELINKING=unknown/PRELINKING=yes/g' prelink-sed >/dev/null
     sudo prelink -amvR
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing trash-cli..."
+    echo -e "\n\033[01;32installing trash-cli...\033[00;37m\n"
     sleep 2
     sudo apt-get install trash-cli
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing stacer..."
+    echo -e "\n\033[01;32installing stacer...\033[00;37m\n"
     sleep 2
     sudo apt install stacer -y
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing Eggs..."
+    echo -e "\n\033[01;32installing Eggs...\033[00;37m\n"
     curl -fsSL https://pieroproietti.github.io/penguins-eggs-ppa/KEY.gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/penguins-eggs.gpg
     echo "deb [arch=$(dpkg --print-architecture)] https://pieroproietti.github.io/penguins-eggs-ppa ./" | sudo tee /etc/apt/sources.list.d/penguins-eggs.list >/dev/null
     sudo apt update -y
     sleep 2
     sudo apt install eggs=9.4.15 -y
     sudo apt-mark hold eggs=9.4.15
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "installing flatpak..."
+    echo -e "\n\033[01;32installing flatpak...\033[00;37m\n"
     sleep 2
     sudo apt install flatpak -y
-    echo "done!"
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
 
-    echo "inserting history format in .bashrc"
+    echo -e "\n\033[01;32inserting history format in .bashrc\033[00;37m\n"
     sed -i "14s/.*/HISTTIMEFORMAT='%Y-%m-%d%T '/" ~/.bashrc
     source ~/.bashrc
-    echo done
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
     echo
     
-    echo "installing broot..."
+    echo -e "\n\033[01;32installing broot...\033[00;37m\n"
     curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
 
     sudo mv broot /usr/local/bin
     sudo chmod +x /usr/local/bin/broot
     source ~/.bashrc
-    broot
-    source ~/.bashrc
     echo "broot was installed!"
-    
-    sleep 2
+    echo -e "\n\033[01;37m\033[01;32m done\033[00;37m\033m\n"
+    sleep 3
     shellCare-22.0.1.sh
 
 fi
