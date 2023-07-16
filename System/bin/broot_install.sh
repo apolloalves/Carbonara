90b629572b7c38649f256ce0c451dd6e04be0e17
apolloalves <apolloalves@gmail.com>
2023-07-16 15:08:46 -0300

    broot_install.sh

---
+++ System/bin/broot_install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#####################################################################
+#                                                                   #
+# Script: removepack.sh For updates packages of system and   #
+# optimizer performance.                                            #
+#                                                                   #
+# Author: Apollo Alves                                              #
+# Date: 02/07/2023                                                  #
+#                                                                   #
+#####################################################################
+
+curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
+
+sudo mv broot /usr/local/bin
+sudo chmod +x /usr/local/bin/broot
+source ~/.bashrc
+
+broot
+
+br --version
+
+
