b1deb3682a550f3c34bb35dcab8e3729e5722063
apolloalves <apolloalves@gmail.com>
2023-08-23 18:46:58 -0300

    unistall.sh

--- unistall.sh
+++ unistall.sh
@@ -17,46 +17,48 @@ if [ -n "$path" ]; then
     ls -lsht $path/System/bin/*.sh
     find $path/System/bin/ -ls | wc -l
 
+
     # echo -e "\n\033[01;32msetting execute permissions on shell files...\033[00;37m"
     # sleep 2
-    sudo rm -rfv $path/*
+    sudo rm -rfv $path
     echo -e "\n\033[01;32mremove Shellcare folder...\033[00;37m\n"
     sleep 2
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+   
 
 
     echo -e "\n\033[01;32munistalling neofetch...\033[00;37m\n"
-    sudo apt purge --automove neofetch -y
+    sudo apt purge neofetch -y
     echo ""
     echo -e "\n\033[01;37m\033[01;32mremoved\033[00;37m\033m"
     echo ""
     echo -e "\n\033[01;32munistalling deborphan...\033[00;37m\n"
-    sudo apt purge --automove deborphan -y
+    sudo apt purge deborphan -y
     echo -e "\n\033[01;37m\033[01;32mremoved\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling preload...\033[00;37m\n"
     sleep 2
-    sudo apt purge --autoremove preload -y
+    sudo apt purge preload -y
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    echo -e "\n\033[01;32minstalling prelink...\033[00;37m\n"
+    echo -e "\n\033[01;32munstalling prelink...\033[00;37m\n"
     sleep 2
-    sudo apt purge --autoremove prelink -y
+    sudo apt purge prelink -y
        echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling trash-cli...\033[00;37m\n"
     sleep 2
-    sudo apt purge --autoremove trash-cli -y 
+    sudo apt purge trash-cli -y 
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32munistalling stacer...\033[00;37m\n"
     sleep 2
-    sudo apt purge --autoremove stacer -y
+    sudo apt purge stacer -y
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
     echo -e "\n\033[01;32minstalling Penguins-eggs...\033[00;37m\n"
-   
-    sudo apt purge --autoremove eggs=9.4.15 -y
+    sudo rm -rfv /etc/apt/trusted.gpg.d/penguins-eggs.gpg
+    sudo apt purge eggs=* 
     echo -e "\n\033[01;32minstalling Penguins-eggs calamares...\033[00;37m\n"
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
@@ -65,6 +67,12 @@ if [ -n "$path" ]; then
     sudo apt purge --autoremove flatpak -y
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
+    find '/bin/' -name "*__*.sh" -exec sudo rm -rfv {} \; | wc -l
+    find '/bin/' -name "shellCare-*.sh" -exec sudo rm -rfv {} \; | wc -l
+    cd 
+    echo 'ShellCare foi removido com sucesso!'
+    echo 'atualizado seus pacotes...'
+    sleep 2 
     sudo apt update -y && sudo apt upgrade -y
     
     echo "done"
