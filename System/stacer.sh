3905d2cfe86a8dc172a6d48c491573b6d13baee3
apolloalves <apolloalves@gmail.com>
2023-08-23 16:28:57 -0300

    stacer.sh - adjusts

--- System/stacer.sh
+++ System/stacer.sh
@@ -2,40 +2,32 @@
 
 MENU='shellCare-23.0.4.sh'
 
-VALIDATE=$(test "y" || "n" != "$stacer_question")     
- echo -n 'Do you want to open stacer ( y/n ) ? or press any key to exit '
- read -r stacer_question
+VALIDATE=$(test "y" || "n" != "$stacer_question")
+echo -n 'Do you want to open stacer ( y/n ) ? or press any key to exit '
+read -r stacer_question
 
- if test "y" = "$stacer_question"
-
-    then
+if test "y" = "$stacer_question"; then
 
       echo "stacer is running..."
       sleep 2
-      stacer &        
+      stacer &
 
-      until ! pgrep -x "stacer" > /dev/null
-      
-      do
+      until ! pgrep -x "stacer" >/dev/null; do
             echo -e '\cAguardando o encerramento do stacer pelo usuário...'
             sleep 2
       done
-    
-      $MENU;
-      
+
+      $MENU
 
 fi
 
- if test "n" = "$stacer_question"
-                  
-            then 
-            $MENU;
-
+if test "n" = "$stacer_question"; then
+      $MENU
 fi
 
 if
       echo "${VALIDATE}"
-      then
-      echo -e "The operation was canceled by the user\n"   
+then
+      echo -e "The operation was canceled by the user\n"
 
 fi
