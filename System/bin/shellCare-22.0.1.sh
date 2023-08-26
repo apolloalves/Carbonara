cfc176da3c95e3fe6e0c107651b518187771e195
apolloalves <apolloalves@gmail.com>
2023-08-26 16:54:03 -0300

    fix version 22.0.1

--- System/bin/shellCare-22.0.1.sh
+++ System/bin/shellCare-22.0.1.sh
@@ -84,15 +84,16 @@
 # echo -e "\033[1;48;05;32;100m   Choose an option from the menu  \033[0m"
 
 ######################################################################################################################################################################################
+NEOFETCH='neofetch --ascii_colors 8 7 --colors 7'
+neofetch_output=$($NEOFETCH)
 
+######################################################################################################################################################################################
 LINE='line.sh'
 echo
-echo -e "\033[1;32;02;40;100m         ShellCare System          \033[0m\033[\033[1;40;02;32;107m          Apollo Alves        \033[1;32;02;40;100m     version  22.0.1     \033[0m"
+echo -e "\033[1;97;100m        ShellCare System          \033[0m\033[1;30;107m         Apollo Alves         \033[1;97;100m        version  23.04.1        \033[0m"
 ######################################################################################################################################################################################
-
-
 echo
-neofetch
+echo -e "$neofetch_output"
 $LINE
 echo -e "\033[1;40;02;32;107m   Choose an option from the menu   \033[0m"
 $LINE
@@ -137,74 +138,117 @@ if [ "$option_choice" != "q" ]; then
         echo -e "\033[05;31mThe option: $option_choice will be executed:\033[00;37m\n"
 
         case "$option_choice" in
-            1)
-                update.sh
-                ;;
-            2)
-                remove.sh
-                remove__oldsnaps.sh
-                ;;
-            3)
-                updateSystem.sh
-                ;;
-            4)
-                gnome-terminal --tab -- bash -c "sudo /bin/performance.sh /; exec bash"
-                ;;
-            5)
-                swapState.sh
-                ;;
-            6)
-                disable__services.sh
-                ;;
-            7)
-                systemAnalyse.sh
-                ;;
-            8)
-                eggsCreate.sh
-                ;;
-            9)
-                echo -e "\nOpening broot...\n"
-                sudo broot -s -p -d --sort-by-date '/mnt/VENTOY/'
-                sleep 1
-                ;;
-            10)
-                btrfs_balance.sh
-                ;;
-            11)
-                clonraidBackups.sh
-                ;;
-            12)
-                stacer.sh
-                ;;
-            13)
-                gnome-terminal --tab -- bash -c "showJournalctl.sh /; exec bash"
-                ;;
-            14)
-                checkSpace
-                ;;
-            15)
-                mysql__fix-1.0.sh
-                ;;
-            16)
-                mysqlDisableService.sh
-                ;;
-            17)
-                reinstallGnomeGdm3
-                ;;
-            18)
-                ext__disk-1.0.sh
-                ;;
-            19)
-                sudo do-release-upgrade
-                ;;
-            20)
-                sudo init 6
-                ;;
-            *)
-                echo -e "\nops!\n"
-                echo -e "\033[01;05;37m'$option_choice' command not found!\033[00m\n"
-                echo -e "Invalid input! Please enter 'y' or 'n'.\n"
-                ;;
+        1)
+            update__pack.sh
+            ;;
+        2)
+            remove__traces.sh
+            ;;
+        3)
+            update__system.sh
+            remove__system.sh
+            ;;
+        4)
+            gnome-terminal --tab -- bash -c "sudo su -c '/bin/optimize__performance.sh /'; exec bash"
+            #   new window
+            #   gnome-terminal -- bash -c "sudo su -c '/bin/performance.sh /'; exec bash"
+            ;;
+        5)
+            sudo su -c "bash /bin/unwanted__pack.sh"
+            ;;
+        6)
+            disable__services.sh
+            ;;
+
+        7)
+            sudo su -c "bash /bin/remove__unused_ppa.sh"
+            ;;
+
+        8)
+            system__analyse.sh
+            ;;
+
+        9)
+            swap__state.sh
+            ;;
+        10)
+            TARGETPATH="/mnt/VENTOY"
+            echo "Checking device..."
+            sleep 2
+            sudo mount /dev/sdc1 $TARGETPATH
+            echo -e "\nOpening broot...\n"
+            gnome-terminal --tab -- bash -c "sudo broot -s -p -d --sort-by-date '/mnt/VENTOY/' ; exec bash"
+            sleep 1
+            ;;
+
+        11)
+            check__space.sh
+            ;;
+        12)
+            stacer__tools.sh
+            ;;
+        13)
+            ext__disk-2.1.sh
+            ;;
+
+        14)
+            eggs__create.sh
+            ;;
+
+        15)
+            sudo show__journalctl.sh
+            ;;
+
+        16)
+            clonraid__backups.sh
+            ;;
+        17)
+            reinstall__gnome-gdm3.sh
+            ;;
+
+        18)
+            mysql__fix-1.0.sh
+            ;;
+
+        19)
+            sudo do-release-upgrade
+            ;;
+
+        20)
+            echo -e "\n\033[01;37m\033[01mwait...\033[00;37m\033[00m\n"
+            echo "verificando a existencia do aptitude..."
+            sudo apt install aptitude -y
+            echo "instalação concluida"
+            echo -e "\nabrindo aptitude..."
+            sleep 2
+            sudo aptitude
+            echo "done"
+            ;;
+        21)
+            software-properties-gtk --open-tab=4
+            echo -e "\n\033[01;37m\033[01;32mdone!\033[00;37m\033m\n"
+            ;;
+
+        22)
+            echo -e "\nGerando lista..\n"
+            sleep 2
+
+            # systemctl list-unit-files --type=service | grep disable
+            systemctl list-unit-files --type=service | grep disable >services && cat -n services
+            # echo -e "\n\033[01;37m\033[01;32mdone!\033[00;37m\033m\n"
+            echo ""
+            $LINE
+            sleep 10
+            ;;
+
+        23)
+
+            reboot__system.sh
+            ;;
+        *)
+            echo -e "\033[01;05;37m'$option_choice' command not found!\033[00m\n"
+            echo -e "Invalid input! Please enter 'y' or 'n'.\n"
+            ;;
         esac
         $MENU
     else
