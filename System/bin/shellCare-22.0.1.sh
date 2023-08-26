6998392f3f2a81ff2cb5e024da22758cabc158db
apolloalves <apolloalves@gmail.com>
2023-08-26 17:12:09 -0300

    fix version 22.0.1

--- System/bin/shellCare-22.0.1.sh
+++ System/bin/shellCare-22.0.1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+source ~/.bashrc
+. ~/.bashrc
+clear
+
 ########################################################################################################################
 #                                                                                                                      #
 # SHELLCARE-22.1.sh                                                                                                                #
@@ -84,18 +88,17 @@
 # echo -e "\033[1;48;05;32;100m   Choose an option from the menu  \033[0m"
 
 ######################################################################################################################################################################################
-NEOFETCH='neofetch --ascii_colors 8 7 --colors 7'
+MENU='shellCare-22.0.1.sh'
+NEOFETCH='neofetch --ascii_colors 8 7 --colors 7 8'
 neofetch_output=$($NEOFETCH)
-
-######################################################################################################################################################################################
 LINE='line.sh'
-echo
+######################################################################################################################################################################################
 echo -e "\033[1;97;100m        ShellCare System          \033[0m\033[1;30;107m         Apollo Alves         \033[1;97;100m        version  23.04.1        \033[0m"
 ######################################################################################################################################################################################
 echo
 echo -e "$neofetch_output"
 $LINE
-echo -e "\033[1;40;02;32;107m   Choose an option from the menu   \033[0m"
+echo -e "\033[01;97m Select an option from the menu: \033[0m"
 $LINE
 echo
 ######################################################################################################################################################################################
@@ -103,33 +106,36 @@ echo
 echo -e "\033[01;32m[\033[01;37m 01\033[01;32m ]\033[00;37m - Update all system packages\033m"
 echo -e "\033[01;32m[\033[01;37m 02\033[01;32m ]\033[00;37m - Remove traces of unused packages from the system\033m"
 echo -e "\033[01;32m[\033[01;37m 03\033[01;32m ]\033[00;37m - Run both\033m"
-echo -e "\033[01;32m[\033[01;37m 04\033[01;32m ]\033[00;37m - Optimize system performance - required sudo\033m"
-echo -e "\033[01;32m[\033[01;37m 05\033[01;32m ]\033[00;37m - Swap State\033m"
-echo -e "\033[01;32m[\033[01;37m 06\033[01;32m ]\033[00;37m - Disable services natives\033m"
-echo -e "\033[01;32m[\033[01;37m 07\033[01;32m ]\033[00;37m - Verify Startup time System \033m"
-echo -e "\033[01;32m[\033[01;37m 08\033[01;32m ]\033[00;37m - Create Penguin's Eggs \033m"
-echo -e "\033[01;32m[\033[01;37m 09\033[01;32m ]\033[00;37m - Open my Penguin's Eggs Files - broot \033m"
-echo -e "\033[01;32m[\033[01;37m 10\033[01;32m ]\033[00;37m - Start volume balancing \033m"
-echo -e "\033[01;32m[\033[01;37m 11\033[01;32m ]\033[00;37m - Open CLONRAID backups\033m"
-echo -e "\033[01;32m[\033[01;37m 12\033[01;32m ]\033[00;37m - Open STACER \033m"
-echo -e "\033[01;32m[\033[01;37m 13\033[01;32m ]\033[00;37m - Replays all of your boot messages 'journalctl -b'"
-echo -e "\033[01;32m[\033[01;37m 14\033[01;32m ]\033[00;37m - Check space disks\033m"
-echo -e "\033[01;32m[\033[01;37m 15\033[01;32m ]\033[00;37m - Start mysql assistent\033m"
-echo -e "\033[01;32m[\033[01;37m 16\033[01;32m ]\033[00;37m - Disable mysql"
-echo -e "\033[01;32m[\033[01;37m 17\033[01;32m ]\033[00;37m - Reinstall GNOME gdm3 \033m"
-echo -e "\033[01;32m[\033[01;37m 18\033[01;32m ]\033[00;37m - Mount Externl Disks"
-echo -e "\033[01;32m[\033[01;37m 19\033[01;32m ]\033[00;37m - Check if a new version of Ubuntu is available \033m"
-echo -e "\033[01;32m[\033[01;37m 20\033[01;32m ]\033[00;37m - Reboot System \033m"
+echo -e "\033[01;32m[\033[01;37m 04\033[01;32m ]\033[00;37m - Optimize system performance - ( root user required )  "
+echo -e "\033[01;32m[\033[01;37m 05\033[01;32m ]\033[00;37m - Remove unwanted packages of system - ( root user required )    "
+echo -e "\033[01;32m[\033[01;37m 06\033[01;32m ]\033[00;37m - Disable services natives   "
+echo -e "\033[01;32m[\033[01;37m 07\033[01;32m ]\033[00;37m - Remove unused PPA on system - ( root user required )   "
+echo -e "\033[01;32m[\033[01;37m 08\033[01;32m ]\033[00;37m - Verify Startup time System"
+echo -e "\033[01;32m[\033[01;37m 09\033[01;32m ]\033[00;37m - Swap State"
+echo -e "\033[01;32m[\033[01;37m 10\033[01;32m ]\033[00;37m - Open my Penguin's Eggs Files - broot file manager  "
+echo -e "\033[01;32m[\033[01;37m 11\033[01;32m ]\033[00;37m - Check space disks"
+echo -e "\033[01;32m[\033[01;37m 12\033[01;32m ]\033[00;37m - Open STACER"
+echo -e "\033[01;32m[\033[01;37m 13\033[01;32m ]\033[00;37m - Mount External Disks"
+echo -e "\033[01;32m[\033[01;37m 14\033[01;32m ]\033[00;37m - Create Penguin's Eggs  "
+echo -e "\033[01;32m[\033[01;37m 15\033[01;32m ]\033[00;37m - Replays all of your boot messages 'journalctl -b'  "
+echo -e "\033[01;32m[\033[01;37m 16\033[01;32m ]\033[00;37m - Open CLONRAID backups"
+echo -e "\033[01;32m[\033[01;37m 17\033[01;32m ]\033[00;37m - Reinstall GNOME gdm3   "
+echo -e "\033[01;32m[\033[01;37m 18\033[01;32m ]\033[00;37m - Start mysql assistent  "
+echo -e "\033[01;32m[\033[01;37m 19\033[01;32m ]\033[00;37m - Check if a new version of Ubuntu is available  "
+echo -e "\033[01;32m[\033[01;37m 20\033[01;32m ]\033[00;37m - Manage packages with aptitude"
+echo -e "\033[01;32m[\033[01;37m 22\033[01;32m ]\033[00;37m - Manage packages and drivers"
+echo -e "\033[01;32m[\033[01;37m 23\033[01;32m ]\033[00;37m - Show disable services list"
+echo -e "\033[01;32m[\033[01;37m 23\033[01;32m ]\033[00;37m - Reboot System"
 
 ######################################################################################################################################################################################
 
-echo -e "\033[01;32m[\033[01;37m q\033[01;32m ]\033[00;37m  - QUIT\033m\n"
+echo -e "\033[01;32m[\033[01;37m q\033[01;32m ]\033[00;37m  - QUIT\033m"
 $LINE
 
-read -p "Chosen option : " option_choice
+read -p "Input option : " option_choice
 $LINE
 
-MENU='shellCare-22.0.1.sh'
+
 valid_option=true
 
 if [ "$option_choice" != "q" ]; then
