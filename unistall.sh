4953f0876fe9b5106cf8da307ec99283eb11453b
apolloalves <apolloalves@gmail.com>
2023-08-26 09:07:34 -0300

    peguins uninstall.sh

--- unistall.sh
+++ unistall.sh
@@ -16,68 +16,62 @@ if [ -n "$path" ]; then
 
     ls -lsht $path/System/bin/*.sh
     find $path/System/bin/ -ls | wc -l
-    
+
     sudo rm -rfv $path
     echo -e "\n\033[01;32mremove Shellcare folders...\033[00;37m\n"
     sleep 2
     find '/bin/' -name "*__*.sh" -exec sudo rm -rfv {} \; | wc -l
     find '/bin/' -name "shellCare-*.sh" -exec sudo rm -rfv {} \; | wc -l
-    sudo rm -rfv $HOME/ShellCare
-    cd $HOME
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    sudo rm -rfv $HOME/ShellCare && cd ~
+
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
     echo -e "\n\033[01;32munistalling neofetch...\033[00;37m\n"
     sudo apt purge neofetch -y
-    echo -e "\n\033[01;37m\033[01;32mremoved\033[00;37m\033m"
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
     echo -e "\n\033[01;32munistalling deborphan...\033[00;37m\n"
     sudo apt purge deborphan -y
-    echo -e "\n\033[01;37m\033[01;32mremoved\033[00;37m\033m"
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
-    echo -e "\n\033[01;32minstalling preload...\033[00;37m\n"
+    echo -e "\n\033[01;32munistalling preload...\033[00;37m\n"
     sleep 2
     sudo apt purge preload -y
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
-    echo -e "\n\033[01;32munstalling prelink...\033[00;37m\n"
+    echo -e "\n\033[01;32munistalling prelink...\033[00;37m\n"
     sleep 2
     sudo apt purge prelink -y
-       echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
-    echo -e "\n\033[01;32minstalling trash-cli...\033[00;37m\n"
+    echo -e "\n\033[01;32munistalling trash-cli...\033[00;37m\n"
     sleep 2
-    sudo apt purge trash-cli -y 
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    sudo apt purge trash-cli -y
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
     echo -e "\n\033[01;32munistalling stacer...\033[00;37m\n"
     sleep 2
     sudo apt purge stacer -y
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
 
     echo -e "\n\033[01;32minstalling Penguins-eggs...\033[00;37m\n"
-    # sudo rm -rfv /etc/apt/trusted.gpg.d/penguins-eggs.gpg
-    sudo apt purge -y eggs=* 
+    sudo rm -rfv /etc/apt/trusted.gpg.d/penguins-eggs.gpg
     sudo rm -rfv /etc/apt/sources.list.d/penguins-eggs.list
-    sudo apt update
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
-
+    sudo apt purge -y eggs=*
+    echo -e "\n\033[01;37m[\033[00;32m \033[01mOK\033[00;32m\033[01;37m ]\033[00m\n"  
+    
     echo -e "\n\033[01;32minstalling flatpak...\033[00;37m\n"
     sleep 2
     sudo apt purge --autoremove flatpak -y
     echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
 
-    cd $HOME
-    pwd
-    echo 'ShellCare foi removido com sucesso!'
 
-    echo 'atualizado seus pacotes...'
+    echo 'ShellCare foi removido com sucesso!'
     sleep 2 
+    echo 'atualizado seus pacotes...'
     sudo apt update -y && sudo apt upgrade -y
-    
+    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+    sleep 2
     echo "done"
-    
-    
-    
-    
 
 fi
