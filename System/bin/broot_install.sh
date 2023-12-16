08d0530e9feb77919660f161ff21ce9fa7458c1b
apolloalves <apolloalves@gmail.com>
2023-12-16 18:00:53 -0300

    Update new comments

--- System/bin/broot_install.sh
+++ System/bin/broot_install.sh
@@ -2,26 +2,36 @@
 
 #####################################################################
 #                                                                   #
-# Script: removepack.sh For updates packages of system and          #
-# optimizer performance.                                            #
-#                                                                   #
+# Script: apititude__manager.sh                                     #
 # Author: Apollo Alves                                              #
-# Date: 02/07/2023                                                  #
+# Date: 15/14/2023                                                  #
 #                                                                   #
 #####################################################################
 
-    echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
-    
-    sudo rm -rfv /usr/local/bin/broot/conf-new-style.hjson
-    sudo rm -rfv /usr/local/bin/broot/system.conf
-    sudo rm -rfv /usr/local/bin/broot
-    
-    source ~/.bashrc
+###################################################################################################################
+#                                                                                                                 #
+# Description:                                                                                                    #
+# This script installs and opens the aptitude package manager. It checks if the user has root privileges and then #
+# installs aptitude. After installation, it opens aptitude for package management.                                #
+#                                                                                                                 #
+##################################################################################################################
 
-    echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
+# Source and initialize bashrc
+source ~/.bashrc
+. ~/.bashrc
 
-    curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
-    sleep 5
-    sudo mv -v broot /usr/local/bin
-    sudo chmod +x /usr/local/bin/broot
-    echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
+echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
+
+sudo rm -rfv /usr/local/bin/broot/conf-new-style.hjson
+sudo rm -rfv /usr/local/bin/broot/system.conf
+sudo rm -rfv /usr/local/bin/broot
+
+source ~/.bashrc
+
+echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
+
+curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
+sleep 5
+sudo mv -v broot /usr/local/bin
+sudo chmod +x /usr/local/bin/broot
+echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
