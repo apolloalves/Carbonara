6dd3f2287f3a7c9668d50fa1ebf779ddb50d119e
apolloalves <apolloalves@gmail.com>
2023-12-17 19:46:36 -0300

    remove sudo command

--- System/bin/unwanted__pack.sh
+++ System/bin/unwanted__pack.sh
@@ -19,21 +19,25 @@
 #                                                                    #
 ######################################################################
 
+# Source and initialize bashrc
+source ~/.bashrc
+. ~/.bashrc
+
 # Check if the user is root
 if [[ $EUID -ne 0 ]]; then
-    echo "This script needs to be run as root."
-    echo "Please execute this with sudo"
-    exit 1
+      echo "This script needs to be run as root."
+      echo "Please execute this with "
+      exit 1
 fi
 
- MENU="LinuxBashCare-10.11.23.sh"
+MENU="LinuxBashCare-10.11.23.sh"
 
 YES="y"
 NO="n"
 
- echo
- echo -n 'Do you want remove unwanted packs of system ( y/n ) ? '
- read -r removeunwanted
+echo
+echo -n 'Do you want remove unwanted packs of system ( y/n ) ? '
+read -r removeunwanted
 
 if [ "$removeunwanted" = "$YES" ]; then
 
@@ -45,19 +49,19 @@ if [ "$removeunwanted" = "$YES" ]; then
       echo -e '************************************************************************************************************'
 
       # remove unwanted packages and purge leftover configuration
-            purge-unwanted() {
+      purge-unwanted() {
             apt autoremove --purge -y \
-            snapd lxd-agent-loader ufw command-not-found \
-            apport alsa-ucm-conf alsa-topology-conf byobu \
-            cloud-init cloud-guest-utils cloud-initramfs-copymods cloud-initramfs-dyn-netconf \
-            landscape-common motd-news-config pollinate popularity-contest ubuntu-advantage-tools \
-            open-iscsi multipath-tools accountsservice cryptsetup-initramfs open-vm-tools;
+                  snapd lxd-agent-loader ufw command-not-found \
+                  apport alsa-ucm-conf alsa-topology-conf byobu \
+                  cloud-init cloud-guest-utils cloud-initramfs-copymods cloud-initramfs-dyn-netconf \
+                  landscape-common motd-news-config pollinate popularity-contest ubuntu-advantage-tools \
+                  open-iscsi multipath-tools accountsservice cryptsetup-initramfs open-vm-tools
             rm -rvf \
-            /root/snap \
-            /etc/pollinate \
-            /etc/cloud \
-            /var/lib/cloud \
-            /var/lib/command-not-found;
+                  /root/snap \
+                  /etc/pollinate \
+                  /etc/cloud \
+                  /var/lib/cloud \
+                  /var/lib/command-not-found
       }
 
       purge-unwanted
@@ -68,13 +72,11 @@ fi
 if [ "$removeunwanted" = "$NO" ]; then
 
       $MENU
-    
+
 elif [ "$removeunwanted" != "$YES" ] && [ "$removeunwanted" != "$NO" ]; then
-    
-    echo -e "\nInvalid input! Please enter 'y' or 'n'.\n"
-    sleep 2
-    $MENU
+
+      echo -e "\nInvalid input! Please enter 'y' or 'n'.\n"
+      sleep 2
+      $MENU
 
 fi
-
-
