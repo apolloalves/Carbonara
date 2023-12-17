00450af273c4a83ab5a269ab3da290c2bdd14d51
apolloalves <apolloalves@gmail.com>
2023-12-17 19:47:56 -0300

    remove sudo command

--- System/bin/update__system.sh
+++ System/bin/update__system.sh
@@ -14,14 +14,14 @@
 #                                                                                                                     #
 # If the user chooses to update the packages, the script will perform the following actions:                          #
 #                                                                                                                     #
-# Will update system packages using sudo apt update --fix-missing command.                                            #
+# Will update system packages using  apt update --fix-missing command.                                            #
 # It will install the base-files, sosreport and ubuntu-server packages.                                               #
-# Will fix broken packages using sudo apt --fix-broken install.                                                       #
-# Will resolve missing dependencies using sudo apt install -f.                                                        #
-# Will upgrade all installed packages using sudo apt upgrade -y.                                                      #
-# Perform a full system upgrade using sudo apt full-upgrade -y.                                                       #
-# Perform a distribution upgrade using sudo apt dist-upgrade.                                                         #
-# Perform a safe upgrade using sudo aptitude safe-upgrade -y.                                                         #
+# Will fix broken packages using  apt --fix-broken install.                                                       #
+# Will resolve missing dependencies using  apt install -f.                                                        #
+# Will upgrade all installed packages using  apt upgrade -y.                                                      #
+# Perform a full system upgrade using  apt full-upgrade -y.                                                       #
+# Perform a distribution upgrade using  apt dist-upgrade.                                                         #
+# Perform a safe upgrade using  aptitude safe-upgrade -y.                                                         #
 # Will check for Flatpak updates using flatpak update -y.                                                             #
 # If the user chooses to clean up system packages, the script will perform the following actions:                     #
 #                                                                                                                     #
@@ -30,9 +30,20 @@
 # It will clean the system recycle bin.                                                                               #
 # In either case, if the user enters invalid input (other than "y" or "n"), the script will display an error message. #
 #                                                                                                                     #
-# It is important to note that this script contains commands that require superuser (sudo) privileges, so it is       #
+# It is important to note that this script contains commands that require superuser () privileges, so it is       #
 # recommended to run it carefully and verify the actions that will be performed before proceeding.                    #
 #######################################################################################################################
+
+# Source and initialize bashrc
+source ~/.bashrc
+. ~/.bashrc
+
+# Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script needs to be run as root."
+    echo "Please execute this with "
+    exit 1
+fi
 
 MENU="LinuxBashCare-10.11.23.sh"
 
