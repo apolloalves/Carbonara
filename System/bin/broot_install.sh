238f2f44ce910e80dbb782294a87385e72d51d72
apolloalves <apolloalves@gmail.com>
2023-12-17 19:26:21 -0300

    remove sudo command

--- System/bin/broot_install.sh
+++ System/bin/broot_install.sh
@@ -20,11 +20,18 @@
 source ~/.bashrc
 . ~/.bashrc
 
+# Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script needs to be run as root."
+    echo "Please execute this with sudo"
+    exit 1
+fi
+
 echo -e "\n\033[01;37m\033[01mremoving previous versions broot...\033[00;37m\033[00m\n"
 
-sudo rm -rfv /usr/local/bin/broot/conf-new-style.hjson
-sudo rm -rfv /usr/local/bin/broot/system.conf
-sudo rm -rfv /usr/local/bin/broot
+rm -rfv /usr/local/bin/broot/conf-new-style.hjson
+rm -rfv /usr/local/bin/broot/system.conf
+rm -rfv /usr/local/bin/broot
 
 source ~/.bashrc
 
@@ -32,6 +39,6 @@ echo -e "\n\033[01;32minstalling broot...\033[00;37m\n"
 
 curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
 sleep 5
-sudo mv -v broot /usr/local/bin
-sudo chmod +x /usr/local/bin/broot
+mv -v broot /usr/local/bin
+chmod +x /usr/local/bin/broot
 echo -e "\n\033[01;37m\033[01;32mdone\033[00;37m\033m"
