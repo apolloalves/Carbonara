21510022661363edc1637f8092df84f407b35c83
apolloalves <apolloalves@gmail.com>
2023-12-17 16:19:57 -0300

    Updated description file: release__update.sh

--- System/bin/release__upgrade.sh
+++ System/bin/release__upgrade.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#########################################################################
-#                                                                       #
-# Script: release__upgrade.sh - Enter a line after each command         #           
-#                                                                       #
-# Author: Apollo Alves                                                  #
-# Date: 14/12/2023                                                      #
-#                                                                       #
-#########################################################################
+################################################################################
+#                                                                              #
+# Script: release__upgrade.sh - Enter a line after each command                #           
+#                                                                              #
+# Author: Apollo Alves                                                         #
+# Date: 14/12/2023                                                             #
+#                                                                              #
+# description: This script checks if there is any version of Ubuntu available  #
+################################################################################
 
 sudo do-release-upgrade
