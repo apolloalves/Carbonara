9253cca528bc1da3c254dfc321180ccf18bddcb0
apolloalves <apolloalves@gmail.com>
2023-12-17 19:36:21 -0300

    remove sudo command

--- System/bin/mysql__fix-1.0.sh
+++ System/bin/mysql__fix-1.0.sh
@@ -45,7 +45,7 @@ source ~/.bashrc
 # Check if the user is root
 if [[ $EUID -ne 0 ]]; then
     echo "This script needs to be run as root."
-    echo "Please execute this with sudo"
+    echo "Please execute this with "
     exit 1
 fi
 
@@ -53,14 +53,14 @@ echo -n 'Would you like to remove mysql from the system? (y/n) '
 read -r mysql_question
 
 if test "y" = "$mysql_question"; then
-    sudo apt remove --purge mysql-server -y
-    sudo apt remove --purge phpmyadmin -y
+     apt remove --purge mysql-server -y
+     apt remove --purge phpmyadmin -y
 
     echo 'stoping services of mysql...'
     /etc/init.d/mysql stop
 
     echo 'Removing traces of packages...'
-    sudo apt remove --purge mysql-common
+     apt remove --purge mysql-common
     echo 'Removing mysql...'
     rm -rfv /var/lib/mysql
     echo 'mysql was removided'
@@ -84,25 +84,25 @@ read -r mysql_reinstall
 if test 'y' = "$mysql_reinstall"; then
 
     echo 'Updating packages of system'
-    sudo apt update -y
+     apt update -y
 
     echo 'display list mysql-server'
     dpkg -l mysql-server
 
     echo "Initilizing Install mysql-server..."
-    sudo apt install mysql-server -y
+     apt install mysql-server -y
     #mysql_secure_installation
     echo "Done!"
     echo "Reinstall Workbench..."
-    sudo snap install mysql-workbench-community
+     snap install mysql-workbench-community
     echo "Fixing error connect database server ..;"
-    sudo snap connect mysql-workbench-community:password-manager-service :password-manager-service
+     snap connect mysql-workbench-community:password-manager-service :password-manager-service
     echo -e '\n\033[00;37m[\033[00;32m OK!\033[00;37m ]\033m\n'
-    sudo service mysql restart
-    sudo systemctl status mysql.service
+     service mysql restart
+     systemctl status mysql.service
     echo -e '\033[05;37mEXECUTE THE COMMAND SOURCE INSIDE MYSQL INSTANCE!\033[00;37m'
     echo ''
-    sudo mysql -u root
+     mysql -u root
     #source $HOME/LinuxBashCare/Mysql/mysql__config.sql
 
 elif test 'n' = "$mysql_reinstall"; then
