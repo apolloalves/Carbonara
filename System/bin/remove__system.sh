5668bd91555cc2d2fa9d0cc5427e068dbc47c0bc
apolloalves <apolloalves@gmail.com>
2023-12-17 16:21:47 -0300

    Updated description file: remove__system.sh

--- System/bin/remove__system.sh
+++ System/bin/remove__system.sh
@@ -2,36 +2,22 @@
 
 #####################################################################
 #                                                                   #
-# Script: REMOVETRACES.sh                                             #
+# Script: REMOVETRACES.sh                                           #
 # Author: Apollo Alves                                              #
-# Date: 14/07/2023                                                  #
+# Date: 16/12/2023                                                  #
 #                                                                   #
 #####################################################################
 
-########################################################################################################################
-#                                                                                                                      #
-# This shell script called "REMOVETRACES.sh" presents a menu for cleaning up packages from the system. Here is a summary #
-# of what it does:                                                                                                     #
-#                                                                                                                      #
-# 1. Define some variables, like MENU and REMOVETRACES, which are the names of other shell scripts.                      #
-#                                                                                                                      #
-# 2. Asks the user if he wants to clean up system packages.                                                            #
-# - The user can answer "y" (yes) or "n" (no).                                                                         #
-#                                                                                                                      #
-# 3. If the user answers "y", the script executes the "$REMOVETRACES" script, which is responsible for cleaning up the   #
-# system packages.                                                                                                     #
-#                                                                                                                      #
-# 4. If the user answers "n", the script displays the message "skipped!" and runs the "$MENU" script, which is         #
-# probably a main menu or next step.                                                                                   #
-#                                                                                                                      #
-# 5. If the user provides an answer other than "y" or "n", the script displays an invalid input message and returns    #
-# to the "$MENU" menu.                                                                                                 #
-#                                                                                                                      #
-# In short, this script presents a menu for cleaning up system packages and runs the cleanup script only if the        #
-# user chooses to do so. Otherwise, it skips cleaning and proceeds to the next step in the menu.                       #
-#                                                                                                                      #
-########################################################################################################################
-
+#####################################################################
+#                                                                   #
+# Remove Traces Script                                              #
+# This script is designed to clean system packages. It prompts the  #
+# user if they want to proceed with the cleaning process. It checks #
+# for root privileges, executes the 'remove__traces.sh' script to   #
+# perform the actual cleaning, and provides appropriate messages.   #
+# Requires root privileges to run.                                  #
+#                                                                   #
+#####################################################################
 
 # Check if the user is root
 if [[ $EUID -ne 0 ]]; then
