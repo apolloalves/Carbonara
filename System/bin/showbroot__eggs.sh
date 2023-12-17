a2bd9d190c4f3bf6d04dfabf29d5edcd1845e1f6
apolloalves <apolloalves@gmail.com>
2023-12-17 19:45:18 -0300

    remove sudo command

--- System/bin/showbroot__eggs.sh
+++ System/bin/showbroot__eggs.sh
@@ -14,20 +14,22 @@
 # Penguin's Eggs Files .isos files using broot as a file manager.                                                   #
 #                                                                                                                   #
 #####################################################################################################################
+# Source and initialize bashrc
+source ~/.bashrc
+. ~/.bashrc
 
 # Check if the user is root
 if [[ $EUID -ne 0 ]]; then
     echo "This script needs to be run as root."
-    echo "Please execute this with sudo"
+    echo "Please execute this with "
     exit 1
 fi
-
 
 
 TARGETPATH="/mnt/VENTOY"
 
 echo "Checking device..."
 sleep 2
-sudo mount /dev/sdc1 $TARGETPATH
+ mount /dev/sdc1 $TARGETPATH
 echo -e "\nOpening broot...\n"
-sudo broot -s -p -d --sort-by-date '/mnt/VENTOY/' 
+ broot -s -p -d --sort-by-date '/mnt/VENTOY/' 
