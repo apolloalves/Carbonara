2bf6d3ce7a75032216fb67be01979942d0158ce1
apolloalves <apolloalves@gmail.com>
2023-12-17 19:45:51 -0300

    remove sudo command

--- System/bin/software__properties.sh
+++ System/bin/software__properties.sh
@@ -7,5 +7,14 @@
 # Description: This script opens the Software Updates window in tab 4 #
 #                                                                     #
 #######################################################################
+# Source and initialize bashrc
+source ~/.bashrc
+. ~/.bashrc
 
-sudo software-properties-gtk --open-tab=4
+# Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script needs to be run as root."
+    echo "Please execute this with "
+    exit 1
+fi
+ software-properties-gtk --open-tab=4
