189c7123cefa0151fcdc1ae2ce56a6eca2ea4d32
apolloalves <apolloalves@gmail.com>
2024-03-30 16:01:40 -0300

    changed var EXT@SA400S3GB

--- System/bin/mount__extDisks.sh
+++ System/bin/mount__extDisks.sh
@@ -7,7 +7,7 @@ if ((EUID != 0)); then
 fi
 #####################################################################
 #                                                                   #
-# Script: mount__extDisks.sh                                        #
+# Script: mount__EXT@SA400S3GBDisks.sh                                        #
 # Author: Apollo Alves                                              #
 # Date: 16/12/2023                                                  #
 #                                                                   #
@@ -15,24 +15,24 @@ fi
 
 ########################################################################################################################
 #                                                                                                                      #
-# This shell script called "ext_disk-1.0.sh" is used to mount and unmount external disks. Here is a summary of what    #
+# This shell script called "EXT@SA400S3GB_disk-1.0.sh" is used to mount and unmount EXT@SA400S3GBernal disks. Here is a summary of what    #
 # it does:                                                                                                             #
 #                                                                                                                      #
-# 1. Asks the user if he wants to mount the disk "EXTST500LM012__CLONRAID".                                           #
+# 1. Asks the user if he wants to mount the disk "EXT@SA400S3GBST500LM012__CLONRAID".                                           #
 # - If the user answers "y" (yes), the script mounts the disk using the command                                        #
-# " mount /dev/sdc3 /mnt/EXTST500LM012__CLONRAID/" and displays a message indicating that it was successfully     #
+# " mount /dev/sdc3 /mnt/EXT@SA400S3GBST500LM012__CLONRAID/" and displays a message indicating that it was successfully     #
 # mounted.                                                                                                             #
 # - If the user answers "n" (no), the script unmounts the disk using the command                                       #
-# " umount /dev/sdc3 /mnt/EXTST500LM012__CLONRAID/ -l" and displays a message indicating that it was successfully #
+# " umount /dev/sdc3 /mnt/EXT@SA400S3GBST500LM012__CLONRAID/ -l" and displays a message indicating that it was successfully #
 # unmounted.                                                                                                           #
 #                                                                                                                      #
 # - If the user provides an answer other than "y" or "n", displays an invalid argument message.                        #
 #                                                                                                                      #
-# 2. Next, the script asks the user if he wants to mount the "VENTOY" disk (probably a USB flash drive).               #
+# 2. NEXT@SA400S3GB, the script asks the user if he wants to mount the "VENTOY" disk (probably a USB flash drive).               #
 # - The process is similar to the one described above, with specific mounting and unmounting commands for the "VENTOY" #
 # disk.                                                                                                                #
 #                                                                                                                      #
-# In general, this script allows the user to mount and unmount external disks such as "EXTST500LM012__CLONRAID" and   #
+# In general, this script allows the user to mount and unmount EXT@SA400S3GBernal disks such as "EXT@SA400S3GBST500LM012__CLONRAID" and   #
 # "VENTOY" based on the answers provided by the user. The "mount" and "umount" commands are used to perform these      #
 # operations.                                                                                                          #
 #                                                                                                                      #
@@ -47,19 +47,19 @@ print_status() {
     fi
 }
 
-printf "\nMount the disk EXTST500LM012__CLONRAID (y/n) ? "
-read -r EXTST500LM012__CLONRAID
+printf "\nMount the disk EXT@SA400S3GB__CLONRAID (y/n) ? "
+read -r EXT@SA400S3GB__CLONRAID
 
-if test "y" = "$EXTST500LM012__CLONRAID"; then
+if test "y" = "$EXT@SA400S3GB__CLONRAID"; then
     echo
-    mount -o ro,noatime /dev/sdc3 /mnt/EXTST500LM012__CLONRAID/
+    mount -o ro,noatime /dev/sdc3 /mnt/EXT@SA400S3GB__CLONRAID/
     print_status
     echo
-elif test "n" = "$EXTST500LM012__CLONRAID"; then
-    umount /dev/sdc3 /mnt/EXTST500LM012__CLONRAID/ -l > /dev/null 2>&1
+elif test "n" = "$EXT@SA400S3GBST500LM012__CLONRAID"; then
+    umount /dev/sdc3 /mnt/EXT@SA400S3GB__CLONRAID/ -l > /dev/null 2>&1
     echo -e '\n\033[00;37m[\033[00;32m OFF!\033[00;37m ]\033m\n'
 
-elif test "y" || "n" != "$EXTST500LM012__CLONRAID"; then
+elif test "y" || "n" != "$EXT@SA400S3GB__CLONRAID"; then
     echo 'invalid arguments!'
 
 fi
@@ -69,20 +69,20 @@ fi
 #################################################################################################################
 
 echo -n "Mount the disk VENTOY ? (y/n) "
-read -r EXTST500LM012__VENTOY
+read -r EXT@SA400S3GB__VENTOY
 
-if test "y" = "$EXTST500LM012__VENTOY"; then
+if test "y" = "$EXT@SA400S3GB__VENTOY"; then
     echo
     mount -o ro,noatime /dev/sdc1 /mnt/VENTOY/
     print_status
     echo
 
-elif test "n" = "$EXTST500LM012__VENTOY"; then
+elif test "n" = "$EXT@SA400S3GB__VENTOY"; then
 echo
     umount /dev/sdc1 /mnt/VENTOY -l > /dev/null 2>&1
     echo -e '\n\033[00;37m[\033[00;32m OFF!\033[00;37m ]\033m\n'
 
-elif test "y" || "n" != "$EXTST500LM012__VENTOY"; then
+elif test "y" || "n" != "$EXT@SA400S3GB__VENTOY"; then
     echo 'invalid arguments!'
 
 fi
